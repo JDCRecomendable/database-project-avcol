87723421976642ffbc8bb06ab2304ad08eb52cdc
JDCRecomendable <JDCRecomendable@protonmail.ch>
2019-09-27 18:50:09 +1200

    Add part of the data filter forms for customer table

--- webfrontend/__init__.py
+++ webfrontend/__init__.py
@@ -10,6 +10,8 @@ This program DOES NOT COME WITH ANY WARRANTY, EXPRESS OR IMPLIED.
 from flask import Flask, request, render_template
 from database.query_constructors import QueryConstructor
 from base.constants import *
+from webfrontend.forms.select_filters import CustomersDataFilterForm, ProductsDataFilterForm
+from webfrontend.forms.select_filters import CustomerOrdersDataFilterForm, CompanyOrderDataFilterForm
 
 
 app = Flask(__name__)
@@ -52,23 +54,38 @@ def get_select_all_records(query_constructor: QueryConstructor):
 @app.route("/customers", methods=["GET", "POST"])
 def show_customers():
     customers_query_constructor.reset()
+    form = CustomersDataFilterForm()
     if request.method == "GET":
-        return render_template("customers.html", selection=get_select_all_records(customers_query_constructor))
+        return render_template("customers.html",
+                               selection=get_select_all_records(customers_query_constructor),
+                               form=form)
 
 
 @app.route("/products", methods=["GET", "POST"])
 def show_products():
+    products_query_constructor.reset()
+    form = ProductsDataFilterForm()
     if request.method == "GET":
-        return render_template("products.html", selection=get_select_all_records(products_query_constructor))
+        return render_template("products.html",
+                               selection=get_select_all_records(products_query_constructor),
+                               form=form)
 
 
 @app.route("/customer-orders", methods=["GET", "POST"])
 def show_customer_orders():
+    customer_orders_query_constructor.reset()
+    form = CustomerOrdersDataFilterForm()
     if request.method == "GET":
-        return render_template("customerOrders.html", selection=get_select_all_records(customer_orders_query_constructor))
+        return render_template("customerOrders.html",
+                               selection=get_select_all_records(customer_orders_query_constructor),
+                               form=form)
 
 
 @app.route("/company-orders", methods=["GET", "POST"])
 def show_company_orders():
+    company_orders_query_constructor.reset()
+    form = CompanyOrderDataFilterForm()
     if request.method == "GET":
-        return render_template("companyOrders.html", selection=get_select_all_records(company_orders_query_constructor))
+        return render_template("companyOrders.html",
+                               selection=get_select_all_records(company_orders_query_constructor),
+                               form=form)
--- webfrontend/forms/select_filters.py
+++ webfrontend/forms/select_filters.py
@@ -7,48 +7,52 @@ Licensed under the GNU General Public License Version 3.
 This program DOES NOT COME WITH ANY WARRANTY, EXPRESS OR IMPLIED.
 """
 
-from flask_wtf import Form
-from wtforms import StringField, RadioField, DateField, SelectField, IntegerField
-from wtforms import validators
+from wtforms import Form
+from wtforms import StringField, RadioField, DateField, BooleanField, IntegerField
+from wtforms.validators import Email, Length
 
 
 class CustomersDataFilterForm(Form):
     # First Name
     first_name_selection = RadioField("Filter by First Name",
-                                      choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+                                      choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                      default="noFilter")
     first_name_string = StringField("First Name")
-    first_name_at_beginning = SelectField("At beginning")
-    first_name_at_end = SelectField("At end")
+    first_name_at_beginning = BooleanField("String at beginning")
+    first_name_at_end = BooleanField("String at end")
 
     # Last Name
     last_name_selection = RadioField("Filter by Last Name",
-                                     choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+                                     choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                     default="noFilter")
     last_name_string = StringField("Last Name")
-    last_name_at_beginning = SelectField("At beginning")
-    last_name_at_end = SelectField("At end")
+    last_name_at_beginning = BooleanField("String at beginning")
+    last_name_at_end = BooleanField("String at end")
 
     # Email Address
     email_address_selection = RadioField("Filter by Email Address",
-                                         choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
-    email_address_string = StringField("Email Address", validators.Email("Invalid email address!"))
+                                         choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                         default="noFilter")
+    email_address_string = StringField("Email Address", validators=[Email("Invalid email address!")])
 
     # Phone Number
-    phone_number_selection = RadioField("Filter by First Name",
-                                        choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
-    phone_number_string = StringField("Phone Number",
-                                      validators.Length(min=9, max=12, message="Invalid phone number length!"))
+    phone_selection = RadioField("Filter by Phone Number",
+                                 choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                 default="noFilter")
+    phone_string = StringField("Phone Number",
+                               validators=[Length(min=9, max=12, message="Invalid phone number length!")])
 
     # Date Registered
     date_registered_selection = RadioField("Filter by Date Registered",
-                                           choices=[("noFilter", "Do not filter"), ("filterExact", "Filter Exact"),
-                                                    ("filterRanged", "Filter by Range")])
-    date_registered_exact_string = DateField("Exact Date Registered")
+                                           choices=[("noFilter", "Do not filter"), ("filter", "Filter by Range")],
+                                           default="noFilter")
     date_registered_lower_limit_string = DateField("Date Registered from")
     date_registered_upper_limit_string = DateField("Date Registered to")
 
     # Location
     location_selection = RadioField("Filter by Location of Customer",
-                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                    default="noFilter")
     location_place_no = StringField("Place No.")
     location_road_name = StringField("Road Name")
     location_city = StringField("City")
@@ -60,28 +64,28 @@ class CustomersDataFilterForm(Form):
 class ProductsDataFilterForm(Form):
     # GTIN-14
     gtin_14_selection = RadioField("Filter by GTIN-14",
-                                   choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
-    gtin14_string = StringField("GTIN-14", validators.Length(min=14, max=14, message="Invalid length!"))
+                                   choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                   default="noFilter")
+    gtin14_string = StringField("GTIN-14", validators=[Length(min=14, max=14, message="Invalid length!")])
 
     # Name of Product
     name_selection = RadioField("Filter by Name",
-                                choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
-    name_at_beginning = SelectField("At beginning")
-    name_at_end = SelectField("At end")
+                                choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                default="noFilter")
+    name_at_beginning = BooleanField("String at beginning")
+    name_at_end = BooleanField("String at end")
 
     # Current Price
     current_price_selection = RadioField("Filter by Current Price",
-                                         choices=[("noFilter", "Do not filter"), ("filterExact", "Filter Exact"),
-                                                  ("filterRanged", "Filter by Range")])
-    current_price_exact_string = IntegerField("Exact Current Price")
+                                         choices=[("noFilter", "Do not filter"), ("filter", "Filter by Range")],
+                                         default="noFilter")
     current_price_lower_limit_string = IntegerField("Current Price minimum")
     current_price_upper_limit_string = IntegerField("Current Price maximum")
 
     # Qty in Stock
     qty_in_stock_selection = RadioField("Filter by Qty in Stock",
-                                        choices=[("noFilter", "Do not filter"), ("filterExact", "Filter Exact"),
-                                                 ("filterRanged", "Filter by Range")])
-    qty_in_stock_exact_string = IntegerField("Exact Qty in Stock")
+                                        choices=[("noFilter", "Do not filter"), ("filter", "Filter by Range")],
+                                        default="noFilter")
     qty_in_stock_lower_limit_string = IntegerField("Qty in Stock minimum")
     qty_in_stock_upper_limit_string = IntegerField("Qty in Stock maximum")
 
@@ -95,18 +99,16 @@ class OrdersDataFilterForm(Form):
     # Date/Time Ordered
     datetime_ordered_selection = RadioField("Filter by Date/Time Ordered",
                                             choices=[("noFilter", "Do not filter"),
-                                                     ("filterExact", "Filter by Exact Date"),
-                                                     ("filterRanged", "Filter by Range of Dates")])
-    datetime_ordered_exact_string = DateField("Exact Date Ordered")
+                                                     ("filter", "Filter by Range of Dates")],
+                                            default="noFilter")
     datetime_ordered_lower_limit_string = DateField("Date Ordered from")
     datetime_ordered_upper_limit_string = DateField("Date Ordered to")
 
     # Delivery Date
     delivery_date_selection = RadioField("Filter by Delivery Date",
                                          choices=[("noFilter", "Do not filter"),
-                                                  ("filterExact", "Filter by Exact Date"),
-                                                  ("filterRanged", "Filter by Range of Dates")])
-    delivery_date_exact_string = DateField("Exact Delivery Date")
+                                                  ("filter", "Filter by Range of Dates")],
+                                         default="noFilter")
     delivery_date_lower_limit_string = DateField("Delivery Date from")
     delivery_date_upper_limit_string = DateField("Delivery Date to")
 
@@ -114,56 +116,60 @@ class OrdersDataFilterForm(Form):
 class CustomerOrdersDataFilterForm(OrdersDataFilterForm):
     # # Customer ID
     # customer_id_selection = RadioField("Filter by Customer ID",
-    #                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+    #                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+    #                                    default="noFilter")
     # customer_id_string = IntegerField("Customer ID")
     #
     # # Customer First Name
     # customer_first_name_selection = RadioField("Filter by Customer First Name",
-    #                                            choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+    #                                            choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+    #                                            default="noFilter")
     # customer_first_name_string = StringField("Customer First Name")
-    # customer_first_name_at_beginning = SelectField("At beginning")
-    # customer_first_name_at_end = SelectField("At end")
+    # customer_first_name_at_beginning = BooleanField("String at beginning")
+    # customer_first_name_at_end = BooleanField("String at end")
     #
     # # Customer Last Name
     # customer_last_name_selection = RadioField("Filter by Customer Last Name",
-    #                                           choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+    #                                           choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+    #                                           default="noFilter")
     # customer_last_name_string = StringField("Customer Last Name")
-    # customer_last_name_at_beginning = SelectField("At beginning")
-    # customer_last_name_at_end = SelectField("At end")
+    # customer_last_name_at_beginning = BooleanField("String at beginning")
+    # customer_last_name_at_end = BooleanField("String at end")
     #
     # # Customer Email Address
     # customer_email_address_selection = RadioField("Filter by Customer Email Address",
-    #                                               choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+    #                                               choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+    #                                               default="noFilter")
     # customer_email_address_string = StringField("Customer Email Address", validators.Email("Invalid email address!"))
 
     # Customer? (continue)
 
     # Delivery Location
     location_selection = RadioField("Filter by Delivery Location",
-                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
+                                    choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                    default="noFilter")
     location_place_no = StringField("Place No.")
     location_road_name = StringField("Road Name")
     location_city = StringField("City")
 
 
-class CompanyOrderDataFilter(OrdersDataFilterForm):
+class CompanyOrderDataFilterForm(OrdersDataFilterForm):
     # Product GTIN-14
     product_gtin14_selection = RadioField("Filter by Product Ordered",
-                                          choices=[("noFilter", "Do not filter"), ("filter", "Filter")])
-    product_gtin14_text = StringField("Product GTIN-14", validators.Length(min=14, max=14, message="Invalid length!"))
+                                          choices=[("noFilter", "Do not filter"), ("filter", "Filter")],
+                                          default="noFilter")
+    product_gtin14_text = StringField("Product GTIN-14", validators=[Length(min=14, max=14, message="Invalid length!")])
 
     # Qty Bought
     qty_bought_selection = RadioField("Filter by Qty Bought",
-                                      choices=[("noFilter", "Do not filter"), ("filterExact", "Filter Exact"),
-                                               ("filterRanged", "Filter by Range")])
-    qty_bought_exact_string = IntegerField("Exact Qty Bought")
+                                      choices=[("noFilter", "Do not filter"), ("filter", "Filter by Range")],
+                                      default="noFilter")
     qty_bought_lower_limit_string = IntegerField("Qty Bought minimum")
     qty_bought_upper_limit_string = IntegerField("Qty Bought maximum")
 
     # Total Price Paid
     price_paid_selection = RadioField("Filter by Price Paid",
-                                      choices=[("noFilter", "Do not filter"), ("filterExact", "Filter Exact"),
-                                               ("filterRanged", "Filter by Range")])
-    price_paid_exact_string = IntegerField("Exact Price Paid")
+                                      choices=[("noFilter", "Do not filter"), ("filter", "Filter by Range")],
+                                      default="noFilter")
     price_paid_lower_limit_string = IntegerField("Price Paid minimum")
     price_paid_upper_limit_string = IntegerField("Price Paid maximum")
