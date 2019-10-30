1fead3af477b148e72a0c051ac0a0b7464d98b7b
JDCRecomendable <JDCRecomendable@protonmail.ch>
2019-10-30 23:58:54 +1300

    Add proper message to show when ID entered are invalid due to foreign key violations

--- webfrontend/constants.py
+++ webfrontend/constants.py
@@ -17,3 +17,5 @@ FLASH_RECORD_NOT_DELETED = "Record not deleted. Any orders pending related to th
 FLASH_RECORD_ID_NO_MATCH = "Record not deleted. ID entered does not match the actual ID."
 FLASH_NOT_ENOUGH_STOCK = "Not enough quantity in stock to fulfill customer order."
 FLASH_ORDER_FULFILLED = "Order fulfilled. Product quantities updated."
+FLASH_INVALID_INPUT_TO_ADD = "Invalid input entered."
+FLASH_INVALID_INPUT_KEY_CONSTRAINT = "Invalid ID(s) entered."
